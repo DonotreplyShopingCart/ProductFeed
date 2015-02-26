97bf809bf1e9ef682f8fc42a174289513f8140d9
DonotreplyShopingCart <DonotreplyShopingCart@gmail.com>
2015-02-26 15:45:43 +0530

    Interface Without Commit

--- ProductComparisor/IProductCatalogService.cs
+++ ProductComparisor/IProductCatalogService.cs
@@ -20,6 +20,8 @@ namespace ProductComparisor
             BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "GetProducts")]
         List<ProductModel> GetProducts();
 
+
+
         [OperationContract]
         [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json,
             BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "GetProductInfoById/{pId}")]
@@ -28,25 +30,4 @@ namespace ProductComparisor
     }
 
 
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    //[DataContract]
-    //public class CompositeType
-    //{
-    //    bool boolValue = true;
-    //    string stringValue = "Hello ";
-
-    //    [DataMember]
-    //    public bool BoolValue
-    //    {
-    //        get { return boolValue; }
-    //        set { boolValue = value; }
-    //    }
-
-    //    [DataMember]
-    //    public string StringValue
-    //    {
-    //        get { return stringValue; }
-    //        set { stringValue = value; }
-    //    }
-    //}
 }
